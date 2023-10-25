2d9eff58291797e2b035ed6854004ef6a5717f50
klaus <oriokiklaus@gmail.com>
2023-10-26 00:19:45 +0300

    refine code to pycodestyle standard

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,22 +3,29 @@
 basic caching
 adding values to keys in dict
 """
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
     """
-    put method adds provided value
-    to provided key
+    Basic Cache class that inherits from BaseCaching
     """
+
     def put(self, key, item):
+        """
+        put method adds provided value
+        to provided key
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        returns value in self.cache_data linked to
-        provided key
+        Must return the value in self.cache_data
+        linked to key
         """
-
         return self.cache_data.get(key, None)
