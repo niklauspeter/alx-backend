7cc90e512145d7928c4b4bd2128e12e23ac884f5
klaus <oriokiklaus@gmail.com>
2023-10-26 00:55:13 +0300

    implement the least frequently used caching replacement protocal

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Least frequently used caching replacement protocol
+"""
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    LFU caching replacement protocol:
+
+    Args:
+        LFUCache ([class]): [basic caching]
+    """
+
+    def __init__(self) -> None:
+        """ initialize the class """
+        self.temp_list = {}
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        while removing the least frequently used
+        item
+        """
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = min(self.temp_list, key=self.temp_list.get)
+                self.temp_list.pop(pop)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+            if not (key in self.temp_list):
+                self.temp_list[key] = 0
+            else:
+                self.temp_list[key] += 1
+
+    def get(self, key):
+        """
+        Get method returns the item listed by key
+        """
+        if (key is None) or not (key in self.cache_data):
+            return None
+        self.temp_list[key] += 1
+        return self.cache_data.get(key)
